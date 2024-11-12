49a353ae019bb2ebc6caede98fc6a3ca23d3f2ce
Lanred-Dev <76853738+Lanred-Dev@users.noreply.github.com>
2024-11-11 21:59:41 -0600

    Begin work on main page.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+    import Hero from "./_index/Hero.svelte";
+    import InfiniteWall from "./_index/InfiniteWall.svelte";
+</script>
+
+<Hero />
+<InfiniteWall />
---
+++ src/routes/_index/Hero.svelte
@@ -0,0 +1,5 @@
+<div class="relative h-[90vh] w-full">
+    <div class="y-center x-center w-full text-center md:left-[10%] md:translate-x-0 md:text-left">
+        <h1 class="text-5xl font-medium md:text-5xl lg:text-6xl">Search for a Veteran</h1>
+    </div>
+</div>
